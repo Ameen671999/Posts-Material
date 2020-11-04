2f80db42a3fe8f1a6fe9084a86c7c94da9724c40
ameenherokhan <ameen671999@gmail.com>
2020-11-04 12:05:57 +0530

    1. Get post

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Post } from '../post.model';
+import { PostService } from '../posts.service'
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+  enteredTitle = '';
+  enteredContent = '';
+
+
+  constructor(private postService:PostService) { }
+
+  ngOnInit() {
+
+  }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postService.addPost( form.value.title, form.value.content);
+    form.resetForm();
+  }
+
+}
